50a6cf7af7e9b67ad985e0fb72ca356f745b13e0
Abilena <abthomas625@gmail.com>
2022-10-11 07:59:29 -0500

    Initial commit

---
+++ ATM Lab/ATM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Lab
+{
+    public class ATM
+    {
+        public List<Account> Accounts { get; set; }
+        public Account currentAccount { get; set; }
+
+        public ATM(List<Account> accounts)
+        {
+            this.Accounts = accounts;
+        }
+        public ATM()
+        {
+            Accounts = new List<Account>();
+        }
+        public void Register()/*string name, string password*/
+        {
+            Console.WriteLine("Enter your name.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your password.");
+            string password = Console.ReadLine();
+
+            Account a = new Account(name, password);
+            Accounts.Add(a);
+        }
+        public void Login()
+        {
+
+        }
+
+        public void Deposit(string name)
+        {
+            Console.WriteLine("How much would you like to withdraw?");
+            double deposit = double.Parse(Console.ReadLine());
+            if (Accounts.Where(a => a.Username == name).Count() > 0)
+            {
+                Account accounts = Accounts.Where(a => a.Username == name).First();
+                accounts.Balance += deposit;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Account not found");
+            }
+        }
+        public void Withdraw(string name, double balance)
+        {            
+            Console.WriteLine("How much would you like to withdraw?");
+            double withdraw = double.Parse(Console.ReadLine());
+
+            if (Accounts.Where(a => a.Username == name).Count() > 0)
+            {
+                Account accounts = Accounts.Where(a => a.Username == name).First();
+                if (withdraw > balance)
+                {
+                    Console.WriteLine("Insufficient funds");
+                }
+                else
+                {
+                    accounts.Balance -= withdraw;
+                }                
+            }
+            else
+            {
+                Console.WriteLine("Account not found");
+            }
+        }
+    }
+}
---
+++ ATM Lab/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ATM_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, and welcome to GC Banking! Will you be registering a new account with us or logging into an existing account?");
+            string input = Console.ReadLine().ToLower();
+
+            List<Account> accounts = new List<Account>();
+            {
+                new Account("Abilena Thomas", "1234");
+                new Account("David Smith", "5678");
+                new Account("Jane Doe", "2468");
+            }
+
+            if(input == "register")
+            {
+                
+            }
+            else if(input == "login")
+            {
+                Console.WriteLine("Enter your name.");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter your password.");
+                string password = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Input invalid. Enter 'register' or 'login.'");
+                return;
+            }
+        }
+    }
+}
